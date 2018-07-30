ece2a9c9ac67fe9a18f35a5960930f0fa015fbac
Daniele Olmisani <mad4j@users.noreply.github.com>
2018-07-31 00:10:04 +0200

    now working

--- quine.cpp
+++ quine.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-int main(int argc, char **argv) {
-	char quote = 34;
-	const char *arr[] = {
- 	"#include <iostream>",
- 	"int main(int argc, char **argv) {",
- 	"	char quote = 34;",
- 	"	const char *arr[] = {",
- 	" 	",
- 	"	};",
- 	"	for(int i = 0; i < 4; ++i)",
- 	"		std::cout << arr[i] << std::endl;",
- 	"	for(int i = 0; i < 14; ++i)",
- 	"		std::cout << arr[4] << quote << arr[i] << quote << ',' << std::endl;",
- 	"	for(int i = 5; i < 14; ++i)",
- 	"		std::cout << arr[i] << std::endl;",
- 	"	return 0;",
- 	"}",
-	};
-	for(int i = 0; i < 4; ++i)
-		std::cout << arr[i] << std::endl;
-	for(int i = 0; i < 14; ++i)
-		std::cout << arr[4] << quote << arr[i] << quote << ',' << std::endl;
-	for(int i = 5; i < 14; ++i)
-		std::cout << arr[i] << std::endl;
-	return 0;
+#include <iomanip>
+
+int main() {
+  const char *s[] = {
+    "#include <iostream>",
+    "#include <iomanip>",
+    "",
+    "int main() {",
+    "  const char *s[] = {",
+    "  };",
+    "  for(int i = 0; i < 5; ++i)",
+    "    std::cout << s[i] << std::endl;",
+    "  for(int i = 0; i < 14; ++i)",
+    "    std::cout << std::setw(4) << ' ' << std::quoted(s[i]) << ',' << std::endl;",
+    "  for(int i = 5; i < 14; ++i)",
+    "    std::cout << s[i] << std::endl;",
+    "  return 0;",
+    "}",
+  };
+  for(int i = 0; i < 5; ++i)
+    std::cout << s[i] << std::endl;
+  for(int i = 0; i < 14; ++i)
+    std::cout << std::setw(4) << ' ' << std::quoted(s[i]) << ',' << std::endl;
+  for(int i = 5; i < 14; ++i)
+    std::cout << s[i] << std::endl;
+  return 0;
 }
