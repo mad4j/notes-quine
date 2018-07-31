160e790568da7bd97624bfa754521b522420ed40
Daniele Olmisani <mad4j@users.noreply.github.com>
2018-07-31 21:28:31 +0200

    added

--- quine.cpp
+++ quine.cpp
@@ -1,29 +1,10 @@
+auto s = R"(
 #include <iostream>
-#include <iomanip>
-
 int main() {
-  const char *s[] = {
-    "    ",
-    "#include <iostream>",
-    "#include <iomanip>",
-    "",
-    "int main() {",
-    "  const char *s[] = {",
-    "  };",
-    "  for(int i = 1; i < 6; ++i)",
-    "    std::cout << s[i] << std::endl;",
-    "  for(int i = 0; i < 15; ++i)",
-    "    std::cout << s[0] << std::quoted(s[i]) << ',' << std::endl;",
-    "  for(int i = 6; i < 15; ++i)",
-    "    std::cout << s[i] << std::endl;",
-    "  return 0;",
-    "}",
-  };
-  for(int i = 1; i < 6; ++i)
-    std::cout << s[i] << std::endl;
-  for(int i = 0; i < 15; ++i)
-    std::cout << s[0] << std::quoted(s[i]) << ',' << std::endl;
-  for(int i = 6; i < 15; ++i)
-    std::cout << s[i] << std::endl;
-  return 0;
+  std::cout << "auto s = R\"(" << s << ")\";" << s;
+}
+)";
+#include <iostream>
+int main() {
+  std::cout << "auto s = R\"(" << s << ")\";" << s;
 }
