843b86f3082061fdd9d7576b7fe33ec36d16215a
Daniele Olmisani <mad4j@users.noreply.github.com>
2018-07-31 06:39:37 +0000

    Update quine.cpp

--- quine.cpp
+++ quine.cpp
@@ -3,26 +3,27 @@
 
 int main() {
   const char *s[] = {
+    "    ",
     "#include <iostream>",
     "#include <iomanip>",
     "",
     "int main() {",
     "  const char *s[] = {",
     "  };",
-    "  for(int i = 0; i < 5; ++i)",
+    "  for(int i = 1; i < 6; ++i)",
     "    std::cout << s[i] << std::endl;",
-    "  for(int i = 0; i < 14; ++i)",
-    "    std::cout << std::setw(4) << ' ' << std::quoted(s[i]) << ',' << std::endl;",
-    "  for(int i = 5; i < 14; ++i)",
+    "  for(int i = 0; i < 15; ++i)",
+    "    std::cout << s[0] << std::quoted(s[i]) << ',' << std::endl;",
+    "  for(int i = 6; i < 15; ++i)",
     "    std::cout << s[i] << std::endl;",
     "  return 0;",
     "}",
   };
-  for(int i = 0; i < 5; ++i)
+  for(int i = 1; i < 6; ++i)
     std::cout << s[i] << std::endl;
-  for(int i = 0; i < 14; ++i)
-    std::cout << std::setw(4) << ' ' << std::quoted(s[i]) << ',' << std::endl;
-  for(int i = 5; i < 14; ++i)
+  for(int i = 0; i < 15; ++i)
+    std::cout << s[0] << std::quoted(s[i]) << ',' << std::endl;
+  for(int i = 6; i < 15; ++i)
     std::cout << s[i] << std::endl;
   return 0;
 }
